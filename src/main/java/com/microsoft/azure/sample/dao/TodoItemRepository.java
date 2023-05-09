15a0960f19747f41e30adc3740fcb88ff657a93e
Florian Walliser <f.walliser@quality-miners.de>
2023-05-09 15:04:14 +0200

    Java Version changed

--- src/main/java/com/microsoft/azure/sample/dao/TodoItemRepository.java
+++ src/main/java/com/microsoft/azure/sample/dao/TodoItemRepository.java
@@ -7,8 +7,6 @@ package com.microsoft.azure.sample.dao;
 
 import com.microsoft.azure.sample.model.TodoItem;
 import com.microsoft.azure.spring.data.cosmosdb.repository.DocumentDbRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TodoItemRepository extends DocumentDbRepository<TodoItem, String> {
 }
